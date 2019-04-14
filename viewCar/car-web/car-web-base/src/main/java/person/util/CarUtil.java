7739d9840ef51a2a052031ed9290b9691a013b19
midnight <374137689@qq.com>
2019-04-14 17:02:19 +0800

    添加手机号中存在看不见的特殊字符正则处理过滤

--- viewCar/car-web/car-web-base/src/main/java/person/util/CarUtil.java
+++ viewCar/car-web/car-web-base/src/main/java/person/util/CarUtil.java
@@ -10,6 +10,7 @@ import person.security.cache.CacheManager;
 import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -382,7 +383,11 @@ public class CarUtil {
         TblFileDetailBean temp = null;
         if(null != maps && maps.size() > 0) {
             for (Map<String, Object> map : maps) {
-                String p = map.get("phone").toString();
+                String p = map.get("phone").toString().trim();
+                Pattern t = Pattern.compile("[\\u007f-\\u009f]|\\u00ad|[\\u0483-\\u0489]|[\\u0559-\\u055a]|\\u058a|[\\u0591-\\u05bd]|\\u05bf|[\\u05c1-\\u05c2]|[\\u05c4-\\u05c7]|[\\u0606-\\u060a]|[\\u063b-\\u063f]|\\u0674|[\\u06e5-\\u06e6]|\\u070f|[\\u076e-\\u077f]|\\u0a51|\\u0a75|\\u0b44|[\\u0b62-\\u0b63]|[\\u0c62-\\u0c63]|[\\u0ce2-\\u0ce3]|[\\u0d62-\\u0d63]|\\u135f|[\\u200b-\\u200f]|[\\u2028-\\u202e]|\\u2044|\\u2071|[\\uf701-\\uf70e]|[\\uf710-\\uf71a]|\\ufb1e|[\\ufc5e-\\ufc62]|\\ufeff|\\ufffc");
+                Matcher m = t.matcher(p);
+                p= m.replaceAll("");
+                phone = m.replaceAll("");
                 if(!map.get("id").toString().equals(id)) {
                     if (flag.equals("xiao")) {
                         if (new BigInteger(phone).subtract(new BigInteger(p)).compareTo(new BigInteger("" + i)) == 0) {
